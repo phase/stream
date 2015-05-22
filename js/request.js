bb802be7039f5c58c7e53721700805b7ffb9cbd6
phase <ph4sesaber@gmail.com>
2015-05-21 20:51:57 -0700

    Add form data

--- js/request.js
+++ js/request.js
@@ -1,12 +1,12 @@
 $("#form").on("submit", function(){
   $.ajax(
     type: "POST"
-    url: "https://docs.google.com/forms/d/FORM-ID/formResponse"
+    url: "https://docs.google.com/forms/d/1M6W7IdCbIgEGn8xQ3ROfiklOG0J6B105NbgPB89qvl4/formResponse"
     data: {
-      'entry.name': $("#name").val()
-      'entry.skype': $("#skype").val()
-      'entry.plugin': $("#plugin-name").val()
-      'entry.description': $("#plugin-description").val()
+      'entry.3': $("#name").val()
+      'entry.4': $("#skype").val()
+      'entry.1': $("#plugin-name").val()
+      'entry.2': $("#plugin-description").val()
     }
   );
 });
