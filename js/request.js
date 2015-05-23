a0f0faaeb5e3201fe391a281a46a69c4756e7cd0
phase <ph4sesaber@gmail.com>
2015-05-22 17:32:34 -0700

    Fix request.js
    fenks @jdf221

--- js/request.js
+++ js/request.js
@@ -1,14 +1,11 @@
-$("#form").submit(function(event){
-  alert("Your request has been submitted!");
-  event.preventDefault();
-  $.ajax(
-    type: "POST"
-    url: "https://docs.google.com/forms/d/1M6W7IdCbIgEGn8xQ3ROfiklOG0J6B105NbgPB89qvl4/formResponse"
-    data: {
-      'entry.1048881380': $("#name").val()
-      'entry.1646269383': $("#skype").val()
-      'entry.1775568240': $("#plugin-name").val()
-      'entry.392196055': $("#plugin-description").val()
-    }
-  );
+$("#form").submit(function(event) {
+    event.preventDefault();
+    $.post("https://docs.google.com/forms/d/1M6W7IdCbIgEGn8xQ3ROfiklOG0J6B105NbgPB89qvl4/formResponse", {
+        'entry.1048881380': $("#name").val(),
+        'entry.1646269383': $("#skype").val(),
+        'entry.1775568240': $("#plugin-name").val(),
+        'entry.392196055': $("#plugin-description").val()
+    }, function() {
+        alert("Your request has been submitted!");
+    });
 });
